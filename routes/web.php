1b468fbfd95c7fd93b775a5d7911955ecb381ee8
murilomoraesalvessilva <murilomoraesgithub@gmail.com>
2026-05-05 16:55:56 -0300

    Configurando routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\ConsultationController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('patient.index');
 });
+
+Route::resource('patients', PatientController::class);
+Route::resource('doctors', DoctorController::class);
+Route::resource('consultations', ConsultationController::class);
